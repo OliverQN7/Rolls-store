9c860d9415eb0f6967f62b26d912ab5120759c97
Ilgiz Usmanov <oliverqni7@gmail.com>
2024-03-19 18:58:10 +0300

    A small fix to the previous commit

--- js/toggleCartStatus.js
+++ js/toggleCartStatus.js
@@ -1,7 +1,7 @@
 function toggleCartStatus() {
   const cartWrapper = document.querySelector(".cart-wrapper");
   const cartEmptyBadge = document.querySelector("[data-cart-empty]");
-  const orderForm = document.querySelector(".order-form");
+  const orderForm = document.querySelector("#order-form");
 
   if (cartWrapper.children.length > 0) {
     cartEmptyBadge.classList.add("none");
